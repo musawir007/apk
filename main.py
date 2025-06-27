2b31c272e0afc7f0d9127b72d48d5e22d8a35f6c
musawir007 <karlapolo143@gmail.com>
2025-06-28 03:25:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from kivy.app import App
 from kivy.uix.label import Label
 
-class HelloApp(App):
+class HelloWorldApp(App):
     def build(self):
-        return Label(text='Hello World from Kivy!')
+        return Label(text="Hello, Kivy World!")
 
-if __name__ == '__main__':
-    HelloApp().run()
+if __name__ == "__main__":
+    HelloWorldApp().run()
